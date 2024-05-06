25c82a4585b7a81554c3d0fc98517fb71e0fd802
GET Academy <terje@kolderup.net>
2024-05-06 11:38:11 +0200

    Initial commit

---
+++ 2024IntroEmne3b/MyConsole.cs
@@ -0,0 +1,12 @@
+﻿namespace _2024IntroEmne3b
+{
+    internal class MyConsole
+    {
+        public static string Ask(string question)
+        {
+            Console.Write(question);
+            string answer = Console.ReadLine();
+            return answer;
+        }
+    }
+}
---
+++ 2024IntroEmne3b/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _2024IntroEmne3b
+{
+    internal class Program
+    {
+        // function Main(args)
+        static void Main(string[] args) 
+        {
+            string name = MyConsole.Ask("Hva heter du? ");
+            string birthYearStr = MyConsole.Ask("I hvilket år er du født? ");
+            int birthYear = Convert.ToInt32(birthYearStr);
+            int age = 2024 - birthYear;
+            string answer = MyConsole.Ask("Har du hatt bursdag i år (j/n)? ");
+            char letter = answer[0];
+            if (letter == 'n')
+            {
+                age--;
+            }
+
+            Console.WriteLine($"Hei, {name}!");
+            Console.WriteLine($"Du er {age} år gammel.");
+        }
+    }
+}
